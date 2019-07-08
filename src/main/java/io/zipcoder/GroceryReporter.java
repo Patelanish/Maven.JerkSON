8d6848517aec853eeb9ac13dfae42c17272a852b
Anish Patel <patelanish223609@gmail.com>
2019-07-08 16:48:36 -0400

    I didn't understand and definitely need a tutor

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,9 +1,14 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.List;
 
 public class GroceryReporter {
     private final String originalFileText;
+    List<Item> itemList;
+    // https://proliferay.com/create-json-by-jackson-api/
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
